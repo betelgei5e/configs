a85e24c153104cffd2bc7f6972ffd784b4a0c6c7
betelgei5e <betelgei5e@proton.me>
2024-10-22 22:20:29 +1100

    firstcommit

---
+++ zsh/aliases.zsh
@@ -0,0 +1,23 @@
+# Personal zsh aliases for betelgei5e betel6eu$e
+
+# Gnome app launching
+alias txtg="gnome-text-editor"
+alias calg="gnome-calculator"
+alias dskg="gnome-disks"
+alias dalg="baobab"
+alias docg="evince"
+alias imgg="eog"
+
+# General app launching
+alias bcat="batcat"
+alias chrome="google-chrome-stable"
+
+# Spell correction
+alias clera="clear"
+alias claer="clear"
+
+# Short-handing switches
+alias killall="killall -v"
+
+# Handy commands
+alias wmcheck="echo $XDG_SESSION_TYPE"
